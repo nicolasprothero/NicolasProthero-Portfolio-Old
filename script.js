a8c14557b72e756a06b6357a387e1de78e2911ff
Nicolas Prothero <45578553+nicolasprothero@users.noreply.github.com>
2022-06-08 23:59:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+
+let strImg = ".img_p";
+let isDone = true;
+
+$(document).ready(function(){
+    isDone = false;
+    $(strImg).delay(1000).fadeIn(800, 'swing')
+    setTimeout(function(){
+        isDone = true;
+    }, 1800);
+        
+    $(".link_p").on("mouseover", function(){
+        if(isDone){
+            isDone = false;
+            $(strImg).hide(0,'swing');
+            strImg = ".img_p" 
+            $(strImg).show(0,'swing')}
+            setTimeout(function(){
+                isDone = true;
+            }, 1000);
+
+            }
+        ),
+
+    $(".link_UF").on("mouseover", function(){
+        if(isDone){
+            isDone = false;
+            $(strImg).hide(0,'swing');
+            strImg = ".img_UF" 
+            $(strImg).show(0,'swing')}
+            setTimeout(function(){
+                isDone = true;
+            }, 1000);
+
+            }
+        ),
+
+    $(".link_travel").on("mouseover", function(){
+        if(isDone){
+            isDone = false;
+            $(strImg).hide(0,'swing');
+            strImg = ".img_travel" 
+            $(strImg).show(0,'swing')}
+            setTimeout(function(){
+                isDone = true;
+            }, 1000);
+
+            }
+        ),
+
+    $(".link_art").on("mouseover", function(){
+        if(isDone){
+            isDone = false;
+            $(strImg).hide(0,'swing');
+            strImg = ".img_art" 
+            $(strImg).show(0,'swing')}
+            setTimeout(function(){
+                isDone = true;
+            }, 1000);
+
+            }
+        ),
+
+    $(".link_plant").on("mouseover", function(){
+        if(isDone){
+            isDone = false;
+            $(strImg).hide(0,'swing');
+            strImg = ".img_plant" 
+            $(strImg).show(0,'swing')}
+            setTimeout(function(){
+                isDone = true;
+            }, 1000);
+
+            }
+        );
+
+    })
